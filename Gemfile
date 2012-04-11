b9b7b060809e41f7d2ae879e18d76494c8130c2d
Declan Frye <deckleberryfrye@gmail.com>
2012-04-11 15:39:02 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'word_scramble'
+gem 'grape'
+
+group :development do
+  gem 'rspec'
+end
---
+++ spec/ws_spec.rb
@@ -0,0 +1,11 @@
+describe "home page" do
+  it "doesn't crash" do
+    pending
+  end
+end
+
+describe "/descramble" do
+  it "returns a list of words" do
+    pending
+  end
+end
